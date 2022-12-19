bc50f0bcece7b71b6ba4b576118c290afe392965
uğur cengiz <ugur_nobre@hotmail.com>
2022-12-19 18:03:33 +0300

    19.12.22

---
+++ Core/ETicaretAPI.Application/Validators/Products/CreateProductValidator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ETicaretAPI.Application.ViewModels.Products;
+using FluentValidation;
+
+namespace ETicaretAPI.Application.Validators.Products
+{
+    public class CreateProductValidator: AbstractValidator<VM_Create_Product>
+    {
+        public CreateProductValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Lütfen ürün adını boş geçmeyiniz.")
+                .MaximumLength(150)
+                .MinimumLength(5)
+                .WithMessage("Lütfen ürün adını 5 ile 150 karakter arasında giriniz.");
+
+            RuleFor(x => x.Stock)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Lütfen stok bilgisini boş geçmeyiniz.")
+                .Must(y => y >= 0)
+                .WithMessage("Stok bilgisini negatif olamaz!");
+
+
+            RuleFor(x => x.Price)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Lütfen Fiyat bilgisini boş geçmeyiniz.")
+                .Must(z => z >= 0)
+                .WithMessage("Fiyat bilgisini negatif olamaz!");
+
+
+        }
+    }
+}
--- Presentation/ETicaretAPI.API/Program.cs
+++ Presentation/ETicaretAPI.API/Program.cs
@@ -1,4 +1,7 @@
+using ETicaretAPI.Application.Validators.Products;
+using ETicaretAPI.Infrastructure.Filters;
 using ETicaretAPI.Persistence;
+using FluentValidation.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +13,12 @@ builder.Services.AddCors(options=>
             .AllowCredentials();
     });
 });
-builder.Services.AddControllers();
+
+builder.Services.AddControllers(options=>options.Filters.Add<ValidationFilter>())
+    .AddFluentValidation(configuration =>
+        configuration.RegisterValidatorsFromAssemblyContaining<CreateProductValidator>())
+    .ConfigureApiBehaviorOptions(options => options.SuppressModelStateInvalidFilter = true);
+    
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
